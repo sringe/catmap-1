5202e900a74da89367115922f30b440c96bd1c1d
Dr. Stefan Ringe <sringe@sh-ln01.stanford.edu>
2018-06-08 15:03:37 -0700

    merged

--- catmap/data/templates.py
+++ catmap/data/templates.py
@@ -253,7 +253,7 @@ def interaction_function(coverages,energies,interaction_vector,F,include_derivat
         # HACK!
         #6 for simplified
         #11 for full
-        hack_list = [6] #[11] #[10] # [5] # idx of H*
+        hack_list = [11] #[3] #[10] # [5] # idx of H*
         if hack_list:
             cvgs = [coverages[j] for j in idxs if j not in hack_list]
         else:
